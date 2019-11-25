4d87b01204f7a7426c8179ae14f9f34389725548
Yangyuqi <chang.ya@husky.neu.edu>
2019-11-24 20:57:18 -0500

    login updated

--- app/src/main/java/com/CS5520/athletier/login/FacebookLogin.java
+++ app/src/main/java/com/CS5520/athletier/login/FacebookLogin.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -16,8 +16,10 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -32,34 +34,41 @@ public class FacebookLogin extends AppCompatActivity {
 
     private CallbackManager mCallbackManager;
     private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
     private static final String TAG = "FACELOG";
-    private Button mFacebookBtn;
-  //  @Override
+    private LoginButton mFacebookBtn;
+    private TextView mTextView;
 
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_login);
-      //  FacebookSdk.sdkInitialize(getApplicationContext());
-       // AppEventsLogger.activateApp(this);
 
             // Initialize Firebase Auth
             mAuth = FirebaseAuth.getInstance();
-
-            mFacebookBtn = (Button) findViewById(R.id.fblogin);
-
-        // Initialize Facebook Login button
-        mCallbackManager = CallbackManager.Factory.create();
-        mFacebookBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                LoginManager.getInstance().logInWithReadPermissions(FacebookLogin.this, Arrays.asList("email", "public_profile"));
+            mUser = mAuth.getCurrentUser();
+            if (mUser == null) {
+                setContentView(R.layout.activity_login);
+                FacebookSdk.sdkInitialize(getApplicationContext());
+                mFacebookBtn = (LoginButton) findViewById(R.id.fblogin);
+                mCallbackManager = CallbackManager.Factory.create();
+                mFacebookBtn.setReadPermissions(Arrays.asList("email"));
+                mTextView = (TextView)findViewById(R.id.textView);
+            } else {
+                Intent accountIntent = new Intent(FacebookLogin.this, MainActivity.class);
+                startActivity(accountIntent);
+            }
+        }
+            public void buttonClicked(View v)
+            {
                 LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>(){
                     @Override
                     public void onSuccess(LoginResult loginResult) {
                         Log.d(TAG, "facebook:onSuccess:" + loginResult);
                         handleFacebookAccessToken(loginResult.getAccessToken());
                     }
+
+
+                    // Initialize Facebook Login button
+
 
                     @Override
                     public void onCancel() {
@@ -74,35 +83,6 @@ public class FacebookLogin extends AppCompatActivity {
                     }
                 });
             }
-        });
-
-        }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            updateUI();
-
-        }
-    }
-    //TODO:Link login page with profile page
-    private void updateUI(){
-            //after login, move to profile page
-        Intent accountIntent = new Intent(FacebookLogin.this,MainActivity.class);
-        startActivity(accountIntent);
-        finish();
-        }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Pass the activity result back to the Facebook SDK
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
-    }
     private void handleFacebookAccessToken(AccessToken token) {
         Log.d(TAG, "handleFacebookAccessToken:" + token);
 
@@ -115,17 +95,45 @@ public class FacebookLogin extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI();
+                            updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
                             Toast.makeText(FacebookLogin.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-                            updateUI();
+                            updateUI(null);
                         }
 
                         // ...
                     }
                 });
+
+
+        }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            updateUI(currentUser);
+
+        }
     }
+    //TODO:Link login page with profile page
+    private void updateUI(FirebaseUser currentUser){
+            //after login, move to profile page
+       mTextView.setText(currentUser.getEmail());
+        finish();
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
