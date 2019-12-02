e2c0a27c5b139e80e55b4cb4064b22cf511e79d7
Yangyuqi <chang.ya@husky.neu.edu>
2019-12-02 01:34:53 -0500

    leader board

--- app/src/main/java/com/CS5520/athletier/ui/Leaderboards/LeaderboardsTabViewModel.java
+++ app/src/main/java/com/CS5520/athletier/ui/Leaderboards/LeaderboardsTabViewModel.java
@@ -1,6 +1,37 @@
 package com.CS5520.athletier.ui.Leaderboards;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
+
 public class LeaderboardsTabViewModel extends ViewModel {
+    FirebaseDatabase database;
+    DatabaseReference reff;
+
+    public void getData(String userInfo) {
+        reff = database.getReference("User");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map<String, String> map = dataSnapshot.getValue(Map.class);
+                String username = map.get("username");
+                String record = map.get("record");
+
+                }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/CS5520/athletier/ui/Teams/TeamsTabViewModel.java
+++ app/src/main/java/com/CS5520/athletier/ui/Teams/TeamsTabViewModel.java
@@ -1,20 +1,44 @@
 package com.CS5520.athletier.ui.Teams;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.CS5520.athletier.Models.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class TeamsTabViewModel extends ViewModel {
     private MutableLiveData<User> user = new MutableLiveData<>();
-
+    FirebaseDatabase database;
+    DatabaseReference reff;
     void findUserWithId(String userId) {
         // TODO: Get the user from the Firebase database...
+        database = FirebaseDatabase.getInstance();
+        reff = database.getReference("User");
+        reff.addValueEventListener(new ValueEventListener() {
+                                       @Override
+                                       public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                           for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                                String user = dataSnapshot.getValue(String.class);
 
-        // TODO: Pass the User to the LiveData stream
-        User dummyUser = new User();
-        user.setValue(dummyUser);
+                                                //then passing user the LiveData stream
+                                           }
+                                       }
+
+                                       @Override
+                                       public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                           }
+                                   });
+
+                // TODO: Pass the User to the LiveData stream
+             //   User dummyUser = new User();
+       // user.setValue(dummyUser);
     }
 
     LiveData<User> getCurrentUser() {
